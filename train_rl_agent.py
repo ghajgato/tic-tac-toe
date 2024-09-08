a05a6738ea1d25a469e1062afbc9684b28dc0ac6
Gergely Hajgató <gergely.hajgato@humda.hu>
2024-09-09 00:16:31 +0200

    slightly tuned hyperparameters

--- train_rl_agent.py
+++ train_rl_agent.py
@@ -1,16 +1,17 @@
 from stable_baselines3 import DQN
+from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.evaluation import evaluate_policy
 
 from XOEnv import XOEnv
 
-trn_env = make_vec_env(XOEnv, env_kwargs={'machine_policy': 'minimax'}, n_envs=4)
+trn_env = make_vec_env(XOEnv, env_kwargs={'machine_policy': 'random'}, n_envs=8, vec_env_cls=DummyVecEnv)
 
-model = DQN("MlpPolicy", trn_env, exploration_fraction=.5, verbose=0)
-model.learn(total_timesteps=1000, progress_bar=True)
+model = DQN("MlpPolicy", trn_env, exploration_fraction=.5, gamma=.98, verbose=0)
+model.learn(total_timesteps=2000000, progress_bar=True)
 model.save('tempo')
 
 eval_env = XOEnv(machine_policy='random')
 print(evaluate_policy(model, eval_env, 100))
 eval_env = XOEnv(machine_policy='minimax')
-print(evaluate_policy(model, eval_env, 1))
+print(evaluate_policy(model, eval_env, 100))
